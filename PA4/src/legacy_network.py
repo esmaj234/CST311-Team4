9e62bac027ef9015b7d4de036ada42b77482f211
valentinahanna <127085756+valentinahanna@users.noreply.github.com>
2023-10-21 00:28:09 -0700

    Update legacy_network.py

--- PA4/src/legacy_network.py
+++ PA4/src/legacy_network.py
@@ -24,57 +24,58 @@ def myNetwork():
 
     info( '*** Add switches\n')
     
-    # Create switches in correct order and set correct IPs
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
     s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
 
-    r5 = net.addHost('r5', cls=Node, ip='10.0.1.0/24')
-    r5.cmd('sysctl -w net.ipv4.ip_forward=1')
+    # Create switches in correct order and set correct IPs
+    r3 = net.addHost('r3', cls=Node, ip='10.0.1.1/24')
+    r3.cmd('sysctl -w net.ipv4.ip_forward=1')
+
     r4 = net.addHost('r4', cls=Node, ip='192.168.1.1/30')
     r4.cmd('sysctl -w net.ipv4.ip_forward=1')
-    r3 = net.addHost('r3', cls=Node, ip='10.0.2.0/24')
-    r3.cmd('sysctl -w net.ipv4.ip_forward=1')
-    
-    
-    # Add static and default routes
-    r3.cmd('ip route add 10.0.1.0/24 via 192.168.1.2 dev r3-eth1')
-    r3.cmd('ip route add  default via 192.168.1.2 dev r3-eth1')
-    
-    r4.cmd('ip route add 10.0.2.0/24 via 192.168.1.1 dev r4-eth1')
-    r4.cmd('ip route add 10.0.1.0/24 via 192.168.1.2 dev r4-eth2')
-    
-    r5.cmd('ip route add 10.0.2.0/24 via 192.168.1.1 dev r5-eth1')
-    r5.cmd('ip route add default via 192.168.1.2 dev r5-eth2')
-    
 
+    r5 = net.addHost('r5', cls=Node, ip='10.0.2.1/24')
+    r5.cmd('sysctl -w net.ipv4.ip_forward=1')
+    
     info( '*** Add hosts\n')
-    h1 = net.addHost('h1', cls=Host, ip='10.0.2.2/24', defaultRoute='via 10.0.2.0')
-    h2 = net.addHost('h2', cls=Host, ip='10.0.2.3/24', defaultRoute='via 10.0.2.0')
-    h3 = net.addHost('h3', cls=Host, ip='10.0.1.2/24', defaultRoute='via 10.0.1.0')
-    h4 = net.addHost('h4', cls=Host, ip='10.0.1.3/24', defaultRoute='via 10.0.1.0')
-    
+    h1 = net.addHost('h1', cls=Host, ip='10.0.1.2/24', defaultRoute='via 10.0.1.1')
+    h2 = net.addHost('h2', cls=Host, ip='10.0.1.3/24', defaultRoute='via 10.0.1.1')
 
-    info( '*** Add links\n')
+    h3 = net.addHost('h3', cls=Host, ip='10.0.2.2/24', defaultRoute='via 10.0.2.1')
+    h4 = net.addHost('h4', cls=Host, ip='10.0.2.3/24', defaultRoute='via 10.0.2.1')
+
+    net.addLink(s1, r3) # r3-eth0
+    net.addLink(r3, r4, intfName1='r3-eth1', params1={'ip': '192.168.1.2/30'}, intfName2='r4-eth0', params2={'ip': '192.168.1.1/30'})
+
     net.addLink(h1, s1)
     net.addLink(h2, s1)
     net.addLink(h3, s2)
     net.addLink(h4, s2)
-    net.addLink(s2, r5)
-    net.addLink(s1, r3)
-    net.addLink(r3, r4, params1={'ip': '192.168.1.2/30'}, params2={'ip': '192.168.1.1/30'})
-    net.addLink(r4, r5, params1={'ip': '192.168.2.3/30'}, params2={'ip': '192.168.2.4/30'})
+
+    net.addLink(s2, r5) # r5-eth0
+    net.addLink(r4, r5, intfName2='r4-eth1', params1={'ip': '192.168.2.1/30'}, intfName1='r5-eth1', params2={'ip': '192.168.2.2/30'})
+
+    net['r3'].cmd('ip route add 10.0.2.0/24 via 192.168.1.1 dev r3-eth1')
+    net['r5'].cmd('ip route add 10.0.1.0/24 via 192.168.2.1 dev r4-eth1')
+
+    net['r4'].cmd('ip route add 10.0.1.0/24 via 192.168.1.2 dev r4-eth0')
+    net['r4'].cmd('ip route add 10.0.2.0/24 via 192.168.2.2 dev r5-eth1')
+
+    net['r3'].cmd('ip route add 192.168.2.0/30 via 192.168.1.1 dev r3-eth1')
+    net['r5'].cmd('ip route add 192.168.1.0/30 via 192.168.2.1 dev r4-eth1')
 
     info( '*** Starting network\n')
-    net.build()
     info( '*** Starting controllers\n')
     for controller in net.controllers:
         controller.start()
 
     info( '*** Starting switches\n')
-    net.get('s2').start([c0])
     net.get('s1').start([c0])
+    net.get('s2').start([c0])
 
     info( '*** Post configure switches and hosts\n')
+
+    net.build()
 
     CLI(net)
     net.stop()
