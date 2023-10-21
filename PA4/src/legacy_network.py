afe62601a9b9a85cfe24ecc82d756a34a3cae105
sentinel49 <126994179+sentinel49@users.noreply.github.com>
2023-10-21 14:54:41 -0700

    Updated comments
    Lenin - Added comments and noted corrections to original code.

--- PA4/src/legacy_network.py
+++ PA4/src/legacy_network.py
@@ -9,6 +9,8 @@ from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from mininet.link import TCLink, Intf
 from subprocess import call
+
+# add import makeTerm
 from mininet.term import makeTerm
 
 def myNetwork():
@@ -25,10 +27,11 @@ def myNetwork():
 
     info( '*** Add switches\n')
     
+    # Moved switches to the top of this block and placed in the correct order
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
     s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
 
-    # Create switches in correct order and set correct IPs
+    # Corrected IP addresses and placed in the correct order
     r3 = net.addHost('r3', cls=Node, ip='10.0.1.1/24')
     r3.cmd('sysctl -w net.ipv4.ip_forward=1')
 
@@ -39,23 +42,32 @@ def myNetwork():
     r5.cmd('sysctl -w net.ipv4.ip_forward=1')
     
     info( '*** Add hosts\n')
+    
+    # Added subnet masks to host IP addresses and corrected default routes
     h1 = net.addHost('h1', cls=Host, ip='10.0.1.2/24', defaultRoute='via 10.0.1.1')
     h2 = net.addHost('h2', cls=Host, ip='10.0.1.3/24', defaultRoute='via 10.0.1.1')
-
     h3 = net.addHost('h3', cls=Host, ip='10.0.2.2/24', defaultRoute='via 10.0.2.1')
     h4 = net.addHost('h4', cls=Host, ip='10.0.2.3/24', defaultRoute='via 10.0.2.1')
 
+    # Links
+    
     net.addLink(s1, r3) # r3-eth0
+    
+    # Added interface names to connections between r3 and r4 - needed for static links
     net.addLink(r3, r4, intfName1='r3-eth1', params1={'ip': '192.168.1.2/30'}, intfName2='r4-eth0', params2={'ip': '192.168.1.1/30'})
 
+    # Links from hosts to switches
     net.addLink(h1, s1)
     net.addLink(h2, s1)
     net.addLink(h3, s2)
     net.addLink(h4, s2)
 
     net.addLink(s2, r5) # r5-eth0
+    
+    # Added interface names to connections between r4 and r5 - needed for static links
     net.addLink(r4, r5, intfName2='r4-eth1', params1={'ip': '192.168.2.1/30'}, intfName1='r5-eth1', params2={'ip': '192.168.2.2/30'})
 
+    # Added static links so r3 can reach r5 through r4 and vice versa
     net['r3'].cmd('ip route add 10.0.2.0/24 via 192.168.1.1 dev r3-eth1')
     net['r5'].cmd('ip route add 10.0.1.0/24 via 192.168.2.1 dev r4-eth1')
 
